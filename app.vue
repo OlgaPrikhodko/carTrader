1788282c1b6f66a5527fed3e2f349389160ccb12
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-06-07 20:10:56 +0300

    :star: add cars cards

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@
     <!-- NAVBAR -->
 
     <!-- CARS PAGE -->
-    <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-8 lg:px-16 pb-16 w-1/3">
+    <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-8 lg:px-16 pb-16 w-2/3">
       <div class="mt-32 flex">
         <!-- CAR SIDE BAR -->
         <div class="shadow border w-64 mr-10 z-30 h-[190px]">
@@ -35,6 +35,34 @@
         <!-- CAR SIDE BAR -->
 
         <!-- CAR CARDS -->
+        <div class="w-full">
+          <!-- CAR CARD -->
+          <div
+            class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+          >
+            <div class="flex h-full">
+              <img 
+                src="https://www.lancia.com/content/dam/lancia/cross/lancia-pu-ra-hpe/new1440x900_sfondo%20scuro.jpg" 
+                alt=""
+                class="w-[300px] h-full"
+              >
+              <div class="p-4 flex flex-col">
+                <div>
+                  <h1 class="text-2xl text-blue-700">
+                    Lancia Pu+Ra HPE
+                  </h1>
+                  <p class="text-grey-700">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  </p>
+                </div>
+
+                <h1 class="mt-auto text-xl">$39,555</h1>
+              </div>
+            </div>
+          </div>
+          <!-- CAR CARD -->
+
+        </div>  
         <!-- CAR CARDS -->
       </div>
     </div>
