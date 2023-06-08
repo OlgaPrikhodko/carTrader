d96804854e42ad4c51219cecdcd3021d8ebb02ba
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-06-08 12:01:14 +0300

    :star: feat: add routing & components for cards

---
+++ components/Car/Card.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+  >
+    <div class="flex h-full">
+      <img
+        src="https://www.lancia.com/content/dam/lancia/cross/lancia-pu-ra-hpe/new1440x900_sfondo%20scuro.jpg"
+        alt=""
+        class="w-[300px] h-full"
+      />
+      <div class="p-4 flex flex-col">
+        <div>
+          <h1 class="text-2xl text-blue-700">Lancia Pu+Ra HPE</h1>
+          <p class="text-grey-700">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+
+        <h1 class="mt-auto text-xl">$39,555</h1>
+      </div>
+    </div>
+  </div>
+</template>
