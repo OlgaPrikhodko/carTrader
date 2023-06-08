bf6bee756a3c2a5f0b2261adc2c5188412cc2a76
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-06-08 13:35:42 +0300

    :star: feat: add route and components for car page

---
+++ components/Car/Detail/Description.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="mt-5">
+    <p class="mb-10">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+      doloribus placeat accusamus tempore possimus eius corrupti quis blanditiis
+      sunt libero consequuntur, asperiores nemo perferendis quibusdam cum vitae
+      neque voluptas reprehenderit.
+    </p>
+  </div>
+</template>
---
+++ components/Car/Detail/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mt-10">
+    <img
+      src="https://www.lancia.com/content/dam/lancia/cross/lancia-pu-ra-hpe/new1440x900_sfondo%20scuro.jpg"
+      class="w-full"
+      alt=""
+    />
+
+    <h1 class="mt-10 text-4xl">Lancia Pu+Ra HPE</h1>
+
+    <div class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between">
+      <div class="flex">
+        <p class="mr-2">5 seats</p>
+        <p class="mr-2">|</p>
+        <p class="mr-2">67,345 miles</p>
+      </div>
+
+      <div>
+        <p class="font-bold text-2xl">$84,777</p>
+      </div>
+    </div>
+  </div>
+</template>
