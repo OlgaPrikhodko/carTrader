796fbfb5b96840e4fd5943ffd920b8fee06edd8e
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-06-12 15:59:41 +0300

    :star: feat: render car listing from cars.json

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -1,22 +1,22 @@
+<script setup>
+const props = defineProps({ car: Object });
+</script>
+
 <template>
   <div
     class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
   >
-    <div class="flex h-full">
-      <img
-        src="https://www.lancia.com/content/dam/lancia/cross/lancia-pu-ra-hpe/new1440x900_sfondo%20scuro.jpg"
-        alt=""
-        class="w-[300px] h-full"
-      />
+    <div class="flex h-full" @click="navigateTo(`/car/${car.name}-${car.id}`)">
+      <img :src="car.url" alt="" class="w-[300px] h-full" />
       <div class="p-4 flex flex-col">
         <div>
-          <h1 class="text-2xl text-blue-700">Lancia Pu+Ra HPE</h1>
+          <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
           <p class="text-grey-700">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            {{ car.description }}
           </p>
         </div>
 
-        <h1 class="mt-auto text-xl">$39,555</h1>
+        <h2 class="mt-auto text-xl">${{ car.price }}</h2>
       </div>
     </div>
   </div>
