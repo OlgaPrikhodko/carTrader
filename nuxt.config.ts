58aa0e384d66923b79a5040b6a36c0fe9caca055
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-06-15 13:52:12 +0300

    :heavy_plus_sign: update: connect with supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "@vueuse/nuxt"],
+  modules: [
+    "@nuxtjs/supabase",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image",
+    "@vueuse/nuxt",
+  ],
   devtools: { enabled: true },
 });
