099918484791b294d9796d9a1b96d63f150605eb
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-06-15 16:18:46 +0300

    :star: feat (profile): logout functionality

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,5 +1,26 @@
 <script setup>
 const user = useSupabaseUser();
+const supabase = useSupabaseClient();
+
+const logout = async () => {
+  const { error } = supabase.auth.signOut();
+  if (error) {
+    console.log(error);
+  }
+
+  try {
+    await $fetch("/api/_supabase/session", {
+      method: "POST",
+      body: { event: "SIGNED_OUT", session: null },
+    });
+  } catch (error) {
+    return console.log(error);
+  }
+
+  user.value = null;
+
+  navigateTo("/");
+};
 </script>
 
 <template>
@@ -9,7 +30,7 @@ const user = useSupabaseUser();
     <NuxtLink class="text-3xl font-mono" to="/">cartrader</NuxtLink>
     <div v-if="user" class="flex">
       <NuxtLink to="/profile/listings" class="mr-5">Profile</NuxtLink>
-      <p class="cursor-pointer">Logout</p>
+      <p class="cursor-pointer" @click="logout">Logout</p>
     </div>
 
     <NuxtLink v-else to="/login">Login</NuxtLink>
